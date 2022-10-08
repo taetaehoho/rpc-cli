bf54b2a8ce9118ab7f708e4cf80b10a38b0b3f10
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2022-10-08 10:35:06 -0500

    add private url support

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+const fs = require("fs");
+require("dotenv").config();
+
+const POKT_PRIVATE_KEY = process.env.POKT_PRIVATE_KEY || "";
+
 var argv = require("yargs/yargs")(process.argv.slice(2))
   .usage("Usage: $0 <cmd> [options]") // usage string of application.
   .command("RPC", "fetches all RPC URLs of specified networks") // describe commands available.
@@ -15,11 +20,17 @@ var argv = require("yargs/yargs")(process.argv.slice(2))
     alias: "n",
     demandOption: true,
   })
+  .option("private", {
+    description: "use private RPC URL",
+    default: false,
+    alias: "p",
+  })
   .option("h", {
     alias: "help",
     description: "display help message",
   })
   .string("networks")
+  .boolean("private")
   .help("help")
   .version("1.0.1", "version", "display version information") // the version string.
   .alias("version", "v")
@@ -27,27 +38,30 @@ var argv = require("yargs/yargs")(process.argv.slice(2))
   .showHelpOnFail(false, "whoops, something went wrong! run with --help").argv;
 
 if (argv.networks.length > 0) {
+  const private = "/v1/lb/" + POKT_PRIVATE_KEY;
   argv.networks.forEach(async (network) => {
-    try {
-      const ret = await axios
-        .get("https://docs.pokt.network/use/public-rpc/")
-        .then((res) => {
-          const $ = cheerio.load(res);
-
-          $(".highlight").each(() => {
-            let text = $(value).text();
-            let link = $(value).attr("href");
-            console.log(text, link);
-            if (link) {
-              console.log(link);
+    const ret = await axios.get("https://docs.pokt.network/use/public-rpc/");
+    const $ = cheerio.load(ret.data);
+    $("table > tbody > tr > td")
+      .find("a")
+      .each((i, link) => {
+        const url = $(link).text();
+        if (url.includes("pokt") && url.includes(network.concat("-rpc"))) {
+          let urlWrite = `${network}`.toUpperCase() + "_RPC_URL=" + `${url} \n`;
+          if (argv.private) {
+            urlWrite =
+              `${network}`.toUpperCase() +
+              "_RPC_URL=" +
+              `${url}` +
+              `${private} \n`;
+          }
+          fs.appendFileSync("urls.txt", urlWrite, (error) => {
+            if (error) {
+              return error;
             }
           });
-
-          console.log("done!");
-        });
-    } catch (error) {
-      console.log(error);
-    }
+        }
+      });
   });
 } else {
   throw Error("Must pass in at least one network");
